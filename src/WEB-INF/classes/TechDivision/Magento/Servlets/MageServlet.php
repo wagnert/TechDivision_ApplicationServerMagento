32284ba1cd7a7ea9c4c95fb67caa25c8f4c8db69
wagnert <t.wagner@techdivision.com>
2013-10-17 08:44:44 +0200

    Refactoring to prepare move MageServlet to servlet container legacy servlets

--- src/WEB-INF/classes/TechDivision/Magento/Servlets/MageServlet.php
+++ src/WEB-INF/classes/TechDivision/Magento/Servlets/MageServlet.php
@@ -9,7 +9,6 @@
  * that is available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
  */
-
 namespace TechDivision\Magento\Servlets;
 
 use TechDivision\Magento\Application\Magento;
@@ -21,13 +20,13 @@ use TechDivision\ServletContainer\Interfaces\ServletConfig;
 use TechDivision\ServletContainer\Servlets\PhpServlet;
 
 /**
- * @package     TechDivision\Magento
- * @copyright   Copyright (c) 2013 <info@techdivision.com> - TechDivision GmbH
- * @license     http://opensource.org/licenses/osl-3.0.php
- *              Open Software License (OSL 3.0)
- * @author      Johann Zelger <jz@techdivision.com>
+ *
+ * @package TechDivision\Magento
+ * @copyright Copyright (c) 2013 <info@techdivision.com> - TechDivision GmbH
+ * @license http://opensource.org/licenses/osl-3.0.php
+ *          Open Software License (OSL 3.0)
+ * @author Johann Zelger <jz@techdivision.com>
  */
-
 class MageServlet extends PhpServlet
 {
 
@@ -41,13 +40,14 @@ class MageServlet extends PhpServlet
     /**
      * Initialize WebApplication and Servlet
      *
-     * @param ServletConfig $config
+     * @param ServletConfig $config            
      * @return void
      */
-    public function init(ServletConfig $config) {
+    public function init(ServletConfig $config)
+    {
         // call parent init
         parent::init($config);
-
+        
         // register WebApplication
         $this->webApplication = new Magento();
     }
@@ -63,158 +63,80 @@ class MageServlet extends PhpServlet
     }
 
     /**
-     * Initialize global files
-     * 
-     * @return void
-     */
-    public function initFiles()
-    {
-        // init query parser
-        $this->getQueryParser()->clear();
-        // iterate all files
-        
-        foreach ($this->getRequest()->getParts() as $part) {
-            // check if filename is given, write and register it
-            if ($part->getFilename()) {
-                // generate temp filename
-                $tempName = tempnam(ini_get('upload_tmp_dir'), 'magento_upload_');
-                // write part
-                $part->write($tempName);
-                // register uploaded file
-                appserver_register_file_upload($tempName);
-                // init error state
-                $errorState = UPLOAD_ERR_OK;
-            } else {
-                // set error state
-                $errorState = UPLOAD_ERR_NO_FILE;
-                // clear tmp file
-                $tempName = '';
-            }
-            // check if file has array info
-            if (preg_match('/^([^\[]+)(\[.+)?/', $part->getName(), $matches)) {
-
-                // get first part group name and array definition if exists
-                $partGroup = $matches[1];
-                $partArrayDefinition = '';
-                if (isset($matches[2])) {
-                    $partArrayDefinition = $matches[2];
-                }
-
-                $this->getQueryParser()->parseKeyValue(
-                    $partGroup.'[name]'.$partArrayDefinition, $part->getFilename()
-                );
-                $this->getQueryParser()->parseKeyValue(
-                    $partGroup.'[type]'.$partArrayDefinition, $part->getContentType()
-                );
-                $this->getQueryParser()->parseKeyValue(
-                    $partGroup.'[tmp_name]'.$partArrayDefinition, $tempName
-                );
-                $this->getQueryParser()->parseKeyValue(
-                    $partGroup.'[error]'.$partArrayDefinition, $errorState
-                );
-                $this->getQueryParser()->parseKeyValue(
-                    $partGroup.'[size]'.$partArrayDefinition, $part->getSize()
-                );
-            }
-        }
-        // set files globals finally.
-        $_FILES = $this->getQueryParser()->getResult();
-    }
-    
-    /**
-     * Initialize globals
+     * (non-PHPdoc)
      *
-     * @return void
+     * @see \TechDivision\ServletContainer\Servlets\PhpServlet::prepareGlobals()
      */
-    public function initGlobals()
+    protected function prepareGlobals(Request $req)
     {
         // if the application has not been called over a vhost configuration append application folder name
-        if (!$this->getServletConfig()->getApplication()->isVhostOf($this->getRequest()->getServerName())) {
-            $this->getRequest()->setServerVar(
+        if (!$this->getServletConfig()->getApplication()->isVhostOf($req->getServerName())) {
+            $req->setServerVar(
                 'SCRIPT_FILENAME',
-                $this->getRequest()->getServerVar('DOCUMENT_ROOT') .
+                $req->getServerVar('DOCUMENT_ROOT') .
                 DIRECTORY_SEPARATOR . $this->getServletConfig()->getApplication()->getName() .
                 DIRECTORY_SEPARATOR . 'index.php'
             );
-            $this->getRequest()->setServerVar(
+            $req->setServerVar(
                 'SCRIPT_NAME',
                 DIRECTORY_SEPARATOR . $this->getServletConfig()->getApplication()->getName() .
                 DIRECTORY_SEPARATOR . 'index.php'
             );
-            $this->getRequest()->setServerVar(
+            $req->setServerVar(
                 'PHP_SELF',
                 DIRECTORY_SEPARATOR . $this->getServletConfig()->getApplication()->getName() .
                 DIRECTORY_SEPARATOR . 'index.php'
             );
         } else {
-            $this->getRequest()->setServerVar(
-                'SCRIPT_FILENAME', $this->getRequest()->getServerVar('DOCUMENT_ROOT') .  DIRECTORY_SEPARATOR . 'index.php'
+            $req->setServerVar(
+                'SCRIPT_FILENAME', $req->getServerVar('DOCUMENT_ROOT') .  DIRECTORY_SEPARATOR . 'index.php'
             );
-            $this->getRequest()->setServerVar('SCRIPT_NAME', '/index.php');
-            $this->getRequest()->setServerVar('PHP_SELF', '/index.php');
+            $req->setServerVar('SCRIPT_NAME', '/index.php');
+            $req->setServerVar('PHP_SELF', '/index.php');
         }
-
-        $_SERVER = $this->getRequest()->getServerVars();
-        $_SERVER['SERVER_PORT'] = NULL;
-
-        // check post type and set params to globals
-        if ($this->getRequest() instanceof PostRequest) {
-            $_POST = $this->getRequest()->getParameterMap();
-            // check if there are get params send via uri
-            parse_str($this->getRequest()->getQueryString(), $_GET);
-        } else {
-            $_GET = $this->getRequest()->getParameterMap();
-        }
-
-        $_REQUEST = $this->getRequest()->getParameterMap();
-
-        foreach (explode('; ', $this->getRequest()->getHeader('Cookie')) as $cookieLine) {
-            list($key, $value) = explode('=', $cookieLine);
-            $_COOKIE[$key] = $value;
-        }
-        
-        // init files global
-        $this->initFiles();
+        parent::prepareGlobals($req);
     }
-
+    
     /**
-     * @param Request $req
-     * @param Response $res
+     *
+     * @param Request $req            
+     * @param Response $res            
      * @throws \Exception
      */
     public function doGet(Request $req, Response $res)
     {
-        // register request and response objects
-        $this->setRequest($req);
-        $this->setResponse($res);
         // start session
-        $this->getRequest()->getSession()->start();
+        $req->getSession()->start();
         // put session object to WebApplication
-        $this->getWebApplication()->setSession($this->getRequest()->getSession());
+        $this->getWebApplication()->setSession($req->getSession());
         // set root dir in WebApplication
         $this->getWebApplication()->setRootDir($this->getServletConfig()->getWebappPath());
         // load WebApplication
         $this->getWebApplication()->load();
         // init globals
-        $this->initGlobals();
+        $this->initGlobals($req);
         // init WebApplication
         $this->getWebApplication()->init();
         // run WebApplication
         $content = $this->getWebApplication()->run();
+        
+        error_log(var_export($_COOKIE, true));
+        
         // set headers
-        $this->setHeaders();
+        $this->addHeaders($res);
         // set content
-        $this->getResponse()->setContent($content);
+        $res->setContent($content);
     }
 
     /**
+     *
      * @see
-     * @param Request $req
-     * @param Response $res
+     *
+     * @param Request $req            
+     * @param Response $res            
      */
     public function doPost(Request $req, Response $res)
     {
         $this->doGet($req, $res);
     }
-
 }
